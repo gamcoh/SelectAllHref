f0d756ad7a9eafd9884f0cfedb85c9743e187552
Gamliel Cohen <gamliel@MacBook-Pro-de-Gamliel-3.local>
2017-04-10 15:19:58 +0200

    clearing the last sel

--- SelectHref.py
+++ SelectHref.py
@@ -5,4 +5,5 @@ import sublime_plugin
 class SelectHrefCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		hrefs = self.view.find_all('href="[^"]*"')
+		self.view.sel().clear()
 		[self.view.sel().add(region) for region in hrefs]
