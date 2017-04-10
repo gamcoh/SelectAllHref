f8aa2219d5c6a48f2825ed517881201b432eada6
Gamliel Cohen <gamliel@MacBook-Pro-de-Gamliel-3.local>
2017-04-10 15:15:02 +0200

    Initial commit

---
+++ SelectHref.py
@@ -0,0 +1,8 @@
+import sublime
+import sublime_plugin
+
+
+class SelectHrefCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		hrefs = self.view.find_all('href="[^"]*"')
+		[self.view.sel().add(region) for region in hrefs]
